e39d4a5ad621d3d93df2a55d667d0aea7dd62b9e
toutouff <63313022+toutouff@users.noreply.github.com>
2024-04-11 14:31:17 +0200

    implementation de la prog fonctionel

--- src/fr/hetic/calculatorfromFolder.java
+++ src/fr/hetic/calculatorfromFolder.java
@@ -2,6 +2,7 @@ package fr.hetic;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import fr.hetic.calculatorfromfile;
 import fr.hetic.folderExplorer;
 
@@ -9,12 +10,16 @@ public class calculatorfromFolder {
     public static void main(String[] args) throws IOException{
         File folder = new File(args[0]);
         File[] files = folderExplorer.listFilesForFolder(folder);
-        for (File file : files) {
-            if (!file.isDirectory() && file.getName().endsWith(".op")) {
+        Arrays.stream(files)
+            .filter(file -> !file.isDirectory() && file.getName().endsWith(".op"))
+            .forEach(file -> {
                 System.out.println(file.getName());
                 String[] path = {file.getPath()};
-                calculatorfromfile.main(path);
-            }
-        }
+                try {
+                    calculatorfromfile.main(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
     }
 }
